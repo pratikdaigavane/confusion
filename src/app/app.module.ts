af8eb11a451211c58227c436d2b74080a737f7dc
Pratik Daigavane <daigavanep@gmail.com>
2019-05-12 17:56:21 +0530

    header and footer updated

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,12 +10,16 @@ import {MatButtonModule, MatCardModule, MatGridListModule} from '@angular/materi
 import { DishdetailComponent } from './dishdetail/dishdetail.component';
 
 import {DishService} from "./services/dish.service";
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     MenuComponent,
-    DishdetailComponent
+    DishdetailComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
