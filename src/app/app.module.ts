dec9c9b41a53782ed7745fbca1fa6a2c7e1b1974
Pratik Daigavane <daigavanep@gmail.com>
2019-06-05 17:30:47 +0530

    angular material updated

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,7 +6,9 @@ import { FlexLayoutModule } from '@angular/flex-layout';
 import { AppComponent } from './app.component';
 import { MenuComponent } from './menu/menu.component';
 import {MatListModule} from '@angular/material/list';
-import {MatButtonModule, MatCardModule, MatGridListModule} from '@angular/material';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatGridListModule } from '@angular/material/grid-list';
 import { DishdetailComponent } from './dishdetail/dishdetail.component';
 
 import {DishService} from "./services/dish.service";
